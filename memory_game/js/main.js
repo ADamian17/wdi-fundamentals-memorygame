886d9b20697cc323851466ffb02d1c9cf083fcd3
Adonis <adonisdesuvikdo@gmail.com>
2019-07-11 22:30:52 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,33 @@
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = "queen";
+
+cardsInPlay.push("cardOne");
+
+console.log("User flipped queen");
+
+var cardTwo = "king";
+
+cardsInPlay.push("cardTwo");
+
+console.log("User flipped king");
+
+
+if ((cardsInPlay.length  === 2 ) && (cardsInPlay[0] === cardsInPlay[1])) {
+
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry, try again.");
+	}
+
+
+
+
+
+
